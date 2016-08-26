951893789a45e2e239c22bf182abab1cc8e4f975
Miguel Pardo <migsub77@gmail.com>
2016-08-25 22:51:23 -0700

    Website
    My current website

---
+++ iframe.js
@@ -0,0 +1,7 @@
+$(document).load(setTimeout(function() {
+
+ return $("#iframeContainer").append($("<iframe>").attr({id: "blogFrame", src: "https://migsadventureblog.wordpress.com/" }));
+}, 4000 ));
+
+
+
